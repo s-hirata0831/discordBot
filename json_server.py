b1651ca781657a77be55485991b382ceb7fd3461
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2024-11-22 22:37:19 +0900

    fix: 微調整

--- json_server.py
+++ json_server.py
@@ -4,14 +4,22 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def receive_data():
-    data = request.get_json()
-    print(f"Received JSON data: {data}")
+    # JSONデータを取得
+    try:
+        data = request.get_json()  # ここでJSONを取得
+        if not data:
+            return "Invalid JSON", 400
 
-    # JSONファイルに書き出し
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False)
+        print(f"Received JSON data: {data}")
 
-    return jsonify({"message": "Data received!"})
+        # JSONファイルに書き出し
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False)
+
+        return jsonify({"message": "Data received!"})
+
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == "__main__":
     app.run(host="160.16.151.12", port=5000)
